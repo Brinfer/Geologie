2d9346b8952ad7be70afe5c0e556b6992acfd98e
Pierre-Louis GAUTIER <pierrelouis.gautier@reseau.eseo.fr>
2021-04-27 22:36:16 +0200

    #5844: ajout de la possibilite de lire les message provenant du host

--- explorations/Wifi/testHostReachable.c
+++ explorations/Wifi/testHostReachable.c
@@ -1,9 +1,9 @@
 /*
  * @file testHostReachable
  *
- * @brief TODO
+ * @brief Permet de tester s'il est possible de se connecter à un server a un port donne.
  *
- * @version 1.0									TODO
+ * @version 1.0
  * @date 27 avr. 2021
  * @author GAUTIER Pierre-Louis
  * @copyright BSD 2-clauses
@@ -36,9 +36,24 @@
 #define DEFAULT_PORT (80) // default http port TODO use STM32MP1 port
 #define MAX_CONNECTION_ATTEMPT (10)
 
-static int setServerAdress(const char p_ipAdress[], const int p_port, struct sockaddr_in* p_serverAdress);
+typedef struct {
+    // TODO
+} Data;
+
+static int setServerAdress(void);
 static int createSocket(void);
-static int connection(const int p_socket, const struct sockaddr_in* p_serverAdress);
+static int closeSocket(void);
+static int connection(void);
+static int run(void);
+static int readMsg(Data* p_data);
+static void displayMsg(Data* p_data);
+static void intHandler(int p_dummy);
+
+static char s_ip[15]; // 15 => Size of 000.000.000.000
+static int s_port;
+static struct sockaddr_in s_serverAdress;
+static int s_socket;
+static int s_keepGoing = 1;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -59,47 +74,57 @@ static int connection(const int p_socket, const struct sockaddr_in* p_serverAdre
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+static void intHandler(int p_dummy) {
+    s_keepGoing = 0;
+}
+
 int main(int argc, char* argv[]) {
     int l_returnValue = EXIT_FAILURE;
 
-    char l_host[15] = DEFAULT_IP; // 15 => Taille de 000.000.000.000
-    int l_port = DEFAULT_PORT;
-    struct sockaddr_in l_serverAdress;
-    int l_socket;
-
     if (argc > 1) {
-        sprintf(l_host, argv[1]);
-        l_port = atoi(argv[2]);
+        sprintf(s_ip, argv[1]);
+        s_port = atoi(argv[2]);
+    } else {
+        sprintf(s_ip, DEFAULT_IP);
+        s_port = DEFAULT_PORT;
     }
 
-    printf("Try to reach %s on the port %d\n", l_host, l_port);
+    printf("Try to reach %s on the port %d\n", s_ip, s_port);
 
-    l_returnValue = setServerAdress(l_host, l_port, &l_serverAdress);
-    if (l_returnValue == EXIT_SUCCESS) {
-        l_socket = createSocket();
+    l_returnValue = setServerAdress();
 
-        if (l_socket >= 0) {
-            l_returnValue = connection(l_socket, &l_serverAdress);
+    if (l_returnValue == EXIT_SUCCESS && s_keepGoing) {
+        l_returnValue = createSocket();
+
+        if (l_returnValue == EXIT_SUCCESS && s_keepGoing) {
+            l_returnValue = connection();
+
+            if (l_returnValue == EXIT_SUCCESS && s_keepGoing) {
+                l_returnValue = run();
+
+            }
         }
-        else {
-            l_returnValue = EXIT_FAILURE;
+
+        if (l_returnValue != EXIT_SUCCESS) {
+            closeSocket(); // By not saving the returned value, we avoid forgetting the error
+        } else {
+            l_returnValue += closeSocket();
         }
     }
 
     return l_returnValue;
 }
 
-static int setServerAdress(const char p_ipAdress[], const int p_port, struct sockaddr_in* p_serverAdress) {
-    const struct hostent* l_hostInfo = gethostbyname(p_ipAdress);
+static int setServerAdress(void) {
+    const struct hostent* l_hostInfo = gethostbyname(s_ip);
     int l_returnValue = EXIT_FAILURE;
 
     if (l_hostInfo == NULL) {
         printf("%sThe host is not reachable%s\n", "\033[41m", "\033[0m");
-    }
-    else {
-        p_serverAdress->sin_port = htons(p_port);
-        p_serverAdress->sin_family = AF_INET;
-        p_serverAdress->sin_addr = *((struct in_addr*) l_hostInfo->h_addr_list[0]);
+    } else {
+        s_serverAdress.sin_port = htons(s_port);
+        s_serverAdress.sin_family = AF_INET;
+        s_serverAdress.sin_addr = *((struct in_addr*) l_hostInfo->h_addr_list[0]);
 
         l_returnValue = EXIT_SUCCESS;
     }
@@ -108,17 +133,32 @@ static int setServerAdress(const char p_ipAdress[], const int p_port, struct soc
 }
 
 static int createSocket(void) {
-    int l_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    s_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int l_returnValue = EXIT_FAILURE;
 
-    if (l_socket < 0) {
+
+    if (s_socket < 0) {
         printf("%sError socket%s\n", "\033[41m", "\033[0m");
-        l_socket = EXIT_FAILURE;
+    } else {
+        l_returnValue = EXIT_SUCCESS;
     }
 
-    return l_socket;
+    return l_returnValue;
 }
 
-static int connection(const int p_socket, const struct sockaddr_in* p_serverAdress) {
+static int closeSocket(void) {
+    int l_returnValue = close(s_socket);
+
+    if (l_returnValue < 0) {
+        l_returnValue = EXIT_FAILURE;
+    } else {
+        l_returnValue = EXIT_SUCCESS;
+    }
+
+    return l_returnValue;
+}
+
+static int connection(void) {
     printf("%sConnection tent to the server%s\n\n", "\033[34m", "\033[0m");
 
     int l_attemptCounter = 0;
@@ -127,24 +167,95 @@ static int connection(const int p_socket, const struct sockaddr_in* p_serverAdre
     while (l_attemptCounter < MAX_CONNECTION_ATTEMPT) {
         l_attemptCounter++;
 
-        if (connect(p_socket, (struct sockaddr*) p_serverAdress, sizeof(*p_serverAdress)) < 0) {
+        if (connect(s_socket, (struct sockaddr*) &s_serverAdress, sizeof(s_serverAdress)) < 0) {
             printf("%s%s%sConnection failure, attempt %d / %d%s\n",
                 "\033[1A", "\033[K", "\033[33m", l_attemptCounter, MAX_CONNECTION_ATTEMPT, "\033[0m");
 
             sleep(1); // Sleep and retry after
-        }
-        else {
+        } else {
             break; // while (l_attemptCounter < MAX_CONNECTION_ATTEMPT)
         }
     }
 
     if (l_attemptCounter < MAX_CONNECTION_ATTEMPT) {
         printf("%s%s%sConnection success after %d attemps%s\n\n", "\033[1A", "\033[K", "\033[32m", l_attemptCounter, "\033[0m");
-    }
-    else {
+        l_returnValue = EXIT_SUCCESS;
+    } else {
         printf("%sLimit on the number of connection attempts exceeded%s\n", "\033[41m", "\033[0m");
+    }
+
+    return l_returnValue;
+}
+
+static int run(void) {
+    printf("\nPress the enter key to exit the program\n");
+
+    int l_returnValue = EXIT_FAILURE;
+    fd_set l_env;
+
+    while (s_keepGoing) {
+        FD_ZERO(&l_env);
+        FD_SET(STDIN_FILENO, &l_env);
+        FD_SET(s_socket, &l_env);
+
+        if (select(FD_SETSIZE, &l_env, NULL, NULL, NULL) == -1) {
+            printf("%sError with %sselect()%s\n", "\033[41m", "\033[21m", "\033[0m");
+            break;
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &l_env)) {
+            /* Keyboard input */
+            printf("%sKeyboard input, close the program%s\n", "\033[43m", "\033[0m");
+            l_returnValue = EXIT_SUCCESS;
+            break;
+
+        } else if (FD_ISSET(s_socket, &l_env)) {
+            Data l_message;
+            l_returnValue = readMsg(&l_message);
+
+            if (l_returnValue != EXIT_SUCCESS) {
+                break;
+            } else {
+                displayMsg(&l_message);
+            }
+        }
+    }
+
+    return l_returnValue;
+}
+
+static int readMsg(Data* p_data) {
+    int l_returnValue = EXIT_FAILURE;
+
+    unsigned int l_quantityReaddean = 0;
+    unsigned int l_quantityToRead = sizeof(Data);
+
+    while (l_quantityToRead > 0) {
+        l_quantityReaddean = read(s_socket, p_data + l_quantityReaddean, l_quantityToRead);
+
+        if (l_quantityReaddean < 0) {
+            printf("%sError when receiving the message%s\n\n", "\033[41m", "\033[0m");
+            break; // l_quantityWritten
+
+        } else if (l_quantityReaddean == 0) {
+            printf("%s%sServer is disconnect%s\n\n", "\033[43m", "\033[30m", "\033[0m");
+            closeSocket();
+            break; // l_quantityWritten
+
+        } else {
+            l_quantityToRead -= l_quantityReaddean;
+        }
+    }
+
+    if (l_quantityToRead == 0) {
+        // NOTE Think to convert correctly int value (ntohl)
         l_returnValue = EXIT_SUCCESS;
     }
 
     return l_returnValue;
+}
+
+static void displayMsg(Data* p_data) {
+    // TODO
+    printf("Get a new message\n");
 }
