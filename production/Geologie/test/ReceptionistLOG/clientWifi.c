6a78297d4daa2476bec4287cea287b99f4ce6a88
nathan <nathan.brient@reseau.eseo.fr>
2021-05-09 19:56:59 +0200

    #6200 REVISION CODE_I1_V1 modification code, test avec ma wifi

--- production/Geologie/test/ReceptionistLOG/clientWifi.c
+++ production/Geologie/test/ReceptionistLOG/clientWifi.c
@@ -34,13 +34,13 @@
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#define DEFAULT_IP "localhost" 
+#define DEFAULT_IP "192.168.1.74" ///addreesse carte en wlan0 
 #define DEFAULT_PORT (12345)
 #define MAX_CONNECTION_ATTEMPT (10)
 
 typedef struct {
     int integer;
-    char data[15];
+    unsigned char data[15];
 }Data;
 
 static int setServerAdress(void);
@@ -48,15 +48,15 @@ static int createSocket(void);
 static int closeSocket(void);
 static int connection(void);
 static int run(void);
-static int readMsg(Data* p_data);
-static void displayMsg(Data* p_data);
-static void intHandler(int p_dummy);
+static int readMsg(Data* data);
+static void displayMsg(Data* data);
+static void intHandler(int dummy);
 
-static char s_ip[16]; // 15 => Size of 000.000.000.000 + EOF
-static int s_port;
-static struct sockaddr_in s_serverAdress;
-static int s_socket;
-static int s_keepGoing = 1;
+static char ip[16]; // 15 => Size of 000.000.000.000 + EOF
+static int port;
+static struct sockaddr_in serverAdress;
+static int mySocket;
+static int keepGoing = 1;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -65,11 +65,11 @@ static int s_keepGoing = 1;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /**
- * @fn static void intHandler(int p_dummy)
- * @brief Met s_keepGoing a 0 pour 
+ * @fn static void intHandler(int dummy)
+ * @brief Met keepGoing a 0 pour 
  */
-static void intHandler(int p_dummy) {
-    s_keepGoing = 0;
+static void intHandler(int dummy) {
+    keepGoing = 0;
 }
 
 /**
@@ -82,42 +82,42 @@ static void intHandler(int p_dummy) {
  */
 
 int main(int argc, char* argv[]) {
-    int l_returnValue = EXIT_FAILURE;
+    int returnValue = EXIT_FAILURE;
     signal(SIGINT, intHandler);                             /// SIGINT qd on fait un ctr c
-                                                            /// appelle intHandler et met s_keepGoing a 0
+                                                            /// appelle intHandler et met keepGoing a 0
 
     if (argc > 1) {                 ///si on met en parametre une ip on va la choisir
-        sprintf(s_ip, argv[1]);     /// on assigne l'ip prise en paramètre à l'adresse ip du socket
-        s_port = atoi(argv[2]); 
+        sprintf(ip, argv[1]);     /// on assigne l'ip prise en paramètre à l'adresse ip du socket
+        port = atoi(argv[2]); 
     } else {                        /// sinon on utilise l'ip de base
-        sprintf(s_ip, DEFAULT_IP);
-        s_port = DEFAULT_PORT;
+        sprintf(ip, DEFAULT_IP);
+        port = DEFAULT_PORT;
     }
 
-    printf("Try to reach %s on the port %d\n", s_ip, s_port);
+    printf("Try to reach %s on the port %d\n", ip, port);
 
-    l_returnValue = setServerAdress();                          ///on configure les paramètres de la socket
+    returnValue = setServerAdress();                          ///on configure les paramètres de la socket
 
-    if (l_returnValue == EXIT_SUCCESS && s_keepGoing) {
-        l_returnValue = createSocket();                         ///on créer la socket
+    if (returnValue == EXIT_SUCCESS && keepGoing) {
+        returnValue = createSocket();                         ///on créer la socket
 
-        if (l_returnValue == EXIT_SUCCESS && s_keepGoing) {
-            l_returnValue = connection();                       ///on connecte la socket
+        if (returnValue == EXIT_SUCCESS && keepGoing) {
+            returnValue = connection();                       ///on connecte la socket
 
-            if (l_returnValue == EXIT_SUCCESS && s_keepGoing) {
-                l_returnValue = run();                          ///on run l'échange de données
+            if (returnValue == EXIT_SUCCESS && keepGoing) {
+                returnValue = run();                          ///on run l'échange de données
 
             }
         }
 
-        if (l_returnValue != EXIT_SUCCESS) {
+        if (returnValue != EXIT_SUCCESS) {
             closeSocket(); // By not saving the returned value, we avoid forgetting the error
         } else {
-            l_returnValue += closeSocket();
+            returnValue += closeSocket();
         }
     }
 
-    return l_returnValue;
+    return returnValue;
 }
 
 /**
@@ -125,20 +125,20 @@ int main(int argc, char* argv[]) {
  * @brief Permet de set une autre adresse ip du socket que celle par defaut
  */
 static int setServerAdress(void) {
-    const struct hostent* l_hostInfo = gethostbyname(s_ip);    /// gethostbyname ??  TODO
-    int l_returnValue = EXIT_FAILURE;
+    const struct hostent* hostInfo = gethostbyname(ip);    /// gethostbyname ??  TODO
+    int returnValue = EXIT_FAILURE;
 
-    if (l_hostInfo == NULL) {                                   /// si l_hostInfo==NULL host pas atteignalble, erreur
+    if (hostInfo == NULL) {                                   /// si hostInfo==NULL host pas atteignalble, erreur
         printf("%sThe host is not reachable%s\n", "\033[41m", "\033[0m");
     } else {
-        s_serverAdress.sin_port = htons(s_port); ///on met le port du serveur dans les parametre pr créer le socket
-        s_serverAdress.sin_family = AF_INET;
-        s_serverAdress.sin_addr = *((struct in_addr*) l_hostInfo->h_addr_list[0]); /// TODO
+        serverAdress.sin_port = htons(port); ///on met le port du serveur dans les parametre pr créer le socket
+        serverAdress.sin_family = AF_INET;
+        serverAdress.sin_addr = *((struct in_addr*) hostInfo->h_addr_list[0]); /// TODO
 
-        l_returnValue = EXIT_SUCCESS;
+        returnValue = EXIT_SUCCESS;
     }
 
-    return l_returnValue;
+    return returnValue;
 }
 
 /**
@@ -146,18 +146,18 @@ static int setServerAdress(void) {
  * @brief permet de créer le socket client
  */
 static int createSocket(void) {
-    s_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);       ///on choisit un socket TCP (SOCK_STREAM) sur IP (AF_INET)
+    mySocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);       ///on choisit un socket TCP (SOCK_STREAM) sur IP (AF_INET)
                                                                 /// IPPROTO_TCP ????? TODO quasiment pareil
-    int l_returnValue = EXIT_FAILURE;                           
+    int returnValue = EXIT_FAILURE;                           
 
 
-    if (s_socket < 0) {                                         /// est a -1 si erreur, socket pas créé
+    if (mySocket < 0) {                                         /// est a -1 si erreur, socket pas créé
         printf("%sError socket%s\n", "\033[41m", "\033[0m");
     } else {
-        l_returnValue = EXIT_SUCCESS;
+        returnValue = EXIT_SUCCESS;
     }
 
-    return l_returnValue;
+    return returnValue;
 }
 /**
  * @fn static int closeSocket(void)
@@ -165,15 +165,15 @@ static int createSocket(void) {
  * 
  */
 static int closeSocket(void) {
-    int l_returnValue = close(s_socket);
+    int returnValue = close(mySocket);
 
-    if (l_returnValue < 0) {
-        l_returnValue = EXIT_FAILURE;
+    if (returnValue < 0) {
+        returnValue = EXIT_FAILURE;
     } else {
-        l_returnValue = EXIT_SUCCESS;
+        returnValue = EXIT_SUCCESS;
     }
 
-    return l_returnValue;
+    return returnValue;
 }
 
 /**
@@ -184,30 +184,30 @@ static int closeSocket(void) {
 static int connection(void) {
     printf("%sConnection tent to the server%s\n\n", "\033[34m", "\033[0m");
 
-    int l_attemptCounter = 0;                                                                           ///compteur de tentatives de connexions    
-    int l_returnValue = EXIT_FAILURE;                                                                   
+    int attemptcounter = 0;                                                                           ///compteur de tentatives de connexions    
+    int returnValue = EXIT_FAILURE;                                                                   
 
-    while (l_attemptCounter < MAX_CONNECTION_ATTEMPT) {
-        l_attemptCounter++;
+    while (attemptcounter < MAX_CONNECTION_ATTEMPT) {
+        attemptcounter++;
 
-        if (connect(s_socket, (struct sockaddr*) &s_serverAdress, sizeof(s_serverAdress)) < 0) {        ///connexion à l'adresse de la socket
+        if (connect(mySocket, (struct sockaddr*) &serverAdress, sizeof(serverAdress)) < 0) {        ///connexion à l'adresse de la socket
             printf("%s%s%sConnection failure, attempt %d / %d%s\n",
-                "\033[1A", "\033[K", "\033[33m", l_attemptCounter, MAX_CONNECTION_ATTEMPT, "\033[0m"); ///TODO ansy code
+                "\033[1A", "\033[K", "\033[33m", attemptcounter, MAX_CONNECTION_ATTEMPT, "\033[0m"); ///TODO ansy code
 
             sleep(1); /// Sleep and retry after
         } else {
-            break; /// while (l_attemptCounter < MAX_CONNECTION_ATTEMPT)
+            break; /// while (attemptcounter < MAX_CONNECTION_ATTEMPT)
         }
     }
 
-    if (l_attemptCounter < MAX_CONNECTION_ATTEMPT) {                                                   
-        printf("%s%s%sConnection success after %d attemps%s\n\n", "\033[1A", "\033[K", "\033[32m", l_attemptCounter, "\033[0m");
-        l_returnValue = EXIT_SUCCESS;
+    if (attemptcounter < MAX_CONNECTION_ATTEMPT) {                                                   
+        printf("%s%s%sConnection success after %d attemps%s\n\n", "\033[1A", "\033[K", "\033[32m", attemptcounter, "\033[0m");
+        returnValue = EXIT_SUCCESS;
     } else {
         printf("%sLimit on the number of connection attempts exceeded%s\n", "\033[41m", "\033[0m");
     }
 
-    return l_returnValue;
+    return returnValue;
 }
 
 /**
@@ -219,91 +219,95 @@ static int connection(void) {
 static int run(void) {
     printf("\nPress the enter key to exit the program\n");
 
-    int l_returnValue = EXIT_FAILURE;
-    fd_set l_env;
+    int returnValue = EXIT_FAILURE;
+    fd_set env;
 
-    while (s_keepGoing) {
+    while (keepGoing) {
         /* https://broux.developpez.com/articles/c/sockets/#LV-C-2 */
         /* https://www.blaess.fr/christophe/2013/12/27/comprendre-le-fonctionnement-de-select/ */
 
-        /// on lui psse l_env une liste a surveiller pour pas que le thread tourne en permanence
+        /// on lui psse env une liste a surveiller pour pas que le thread tourne en permanence
 
-        FD_ZERO(&l_env);                                                                /// Pas compris TODO, on la remet à 0
-        FD_SET(STDIN_FILENO, &l_env);                                                   /// Pas compris //on demande a surveiller STDIN_FILENO, entrée standard pour le terminal
-        FD_SET(s_socket, &l_env);                                                       /// Pas compris //on demande a surveiller s_socket, si qqn touche au socket
+        FD_ZERO(&env);                                                                /// Pas compris TODO, on la remet à 0
+        FD_SET(STDIN_FILENO, &env);                                                   /// Pas compris //on demande a surveiller STDIN_FILENO, entrée standard pour le terminal
+        FD_SET(mySocket, &env);                                                       /// Pas compris //on demande a surveiller socket, si qqn touche au socket
 
         
-        if (select(FD_SETSIZE, &l_env, NULL, NULL, NULL) == -1) {                       //select dans dans un thread met en pause le thread, attend qu'une condition soit faite pr le reveiller
+        if (select(FD_SETSIZE, &env, NULL, NULL, NULL) == -1) {                       //select dans dans un thread met en pause le thread, attend qu'une condition soit faite pr le reveiller
             printf("%sError with %sselect()%s\n", "\033[41m", "\033[21m", "\033[0m");
             break;
         }
     
-        if (FD_ISSET(STDIN_FILENO, &l_env)) {   ///Regarder ui a été réveillé ? regarder si STDIN_FILENO a reveiller le thread
+        if (FD_ISSET(STDIN_FILENO, &env)) {   ///Regarder ui a été réveillé ? regarder si STDIN_FILENO a reveiller le thread
             /* Keyboard input */
             printf("%sKeyboard input, close the program%s\n", "\033[43m", "\033[0m");
-            l_returnValue = EXIT_SUCCESS;
+            returnValue = EXIT_SUCCESS;
             break;
 
-        } else if (FD_ISSET(s_socket, &l_env)) { /// regardrer si le socket a été touché, si on a recu de l'info
-            Data l_message;
-            l_returnValue = readMsg(&l_message);
+        } else if (FD_ISSET(mySocket, &env)) { /// regardrer si le socket a été touché, si on a recu de l'info
+            Data message;
+            returnValue = readMsg(&message); /// on lit les info du socket et on les met dans message
 
-            if (l_returnValue != EXIT_SUCCESS) {
+            if (returnValue != EXIT_SUCCESS) {
                 break;
             } else {
-                displayMsg(&l_message);
+                displayMsg(&message);
             }
         }
     }
 
-    return l_returnValue;
+    return returnValue;
 }
 
 /**
- * @fn static int readMsg(Data* p_data)
+ * @fn static int readMsg(Data* data)
  * @brief Lis le message sur le socket
  * 
  * @param Data Donnee contenant les informations qui ont ete recues
  */
 
 //si on se déconnecte on le lit avec select et on arrive ici et le read retournera 0, ce qui veut dire qu'on est déconnecté
-static int readMsg(Data* p_data) {
-    int l_returnValue = EXIT_FAILURE;
+static int readMsg(Data* data) {
+    int returnValue = EXIT_FAILURE;
 
-    unsigned int l_quantityReaddean = 0; /// TODO a commenter
-    unsigned int l_quantityToRead = sizeof(Data);
+    unsigned int quantityReaddean = 0; /// TODO a commenter
+    unsigned int quantityToRead = sizeof(Data);
 
-    while (l_quantityToRead > 0) {
-        l_quantityReaddean = read(s_socket, p_data + l_quantityReaddean, l_quantityToRead); ///read ,e garantit pas quil va lire tous les bits
+    while (quantityToRead > 0) {
+        quantityReaddean = read(mySocket, data + quantityReaddean, quantityToRead); ///read ,e garantit pas quil va lire tous les bits
                                                                                             /// un octet une adresse mémoire
                                                                                             /// si read retourne 0 : deconnecter
                                                                                             // si read retourne 0 : erreur
         
-        if (l_quantityReaddean < 0) {   
+        if (quantityReaddean < 0) {   
             printf("%sError when receiving the message%s\n\n", "\033[41m", "\033[0m");
             break; // l_quantityWritten
 
-        } else if (l_quantityReaddean == 0) {
+        } else if (quantityReaddean == 0) {
             printf("%s%sServer is disconnect%s\n\n", "\033[43m", "\033[30m", "\033[0m");
             closeSocket();
             break; // l_quantityWritten
 
         } else {
-            l_quantityToRead -= l_quantityReaddean;
+            quantityToRead -= quantityReaddean;
         }
     }
 
-    if (l_quantityToRead == 0) { //convertir les message pour mieux les afficher, bien
+    if (quantityToRead == 0) { //convertir les message pour mieux les afficher, bien
         // NOTE Think to convert correctly int value (ntohl)
-        l_returnValue = EXIT_SUCCESS;
+        returnValue = EXIT_SUCCESS;
     }
 
-    return l_returnValue;
+    return returnValue;
 }
 
-static void displayMsg(Data* p_data) {
+static void displayMsg(Data* data) {
     // TODO
     static int l_counter = 1;
     printf("Get a new message %d\n", l_counter);
+    printf("on a recu la chaine de caractere %s \n", data->data);
+        
+    printf("On a recu l'entier %i \n", data->integer);
+
     l_counter++;
 }
