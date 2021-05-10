37a6eaff5da7d927a18e85139bffff08f3e7f795
gabriel <gabriel.lecenne@reseau.eseo.fr>
2021-05-10 21:48:36 +0200

    #6268 CREAT CODE_I1 receivers.h

---
+++ production/Geologie/src/Receivers/receivers.h
@@ -0,0 +1,28 @@
+/**
+ * @file receivers.h
+ *
+ * @brief 
+ * @version 1.0.1
+ * @date 5 mai 2021
+ * @author LECENNE Gabriel
+ * @copyright BSD 2-clauses
+ *
+ */
+
+#ifndef RECEIVERS_H_
+#define RECEIVERS_H_
+
+/* \struct Receivers_t
+ * \brief  Structure de receivers définie dans receivers.c
+ */
+typedef struct {
+} Receivers_t;
+
+
+extern Receivers_t * Receivers_new();
+extern void Receivers_free(Receivers_t * this);
+extern void Receivers_start(Receivers_t * this);
+extern void Receivers_stop(Receivers_t * this);
+
+
+#endif /* RECEIVERS_H_ */
