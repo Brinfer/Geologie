b2d9b8492b889a19689ac89d1612cf186cae1f6a
Brinfer <gautierpierrelouis6@gmail.com>
2021-05-10 16:57:29 +0200

    #6208 REVISION Geologie, ajout de test unitaire pour la conversion de BeaconData en tableau d'octect

--- production/Geologie/test/ReceptionistLOG/translator_test.c
+++ production/Geologie/test/ReceptionistLOG/translator_test.c
@@ -44,12 +44,75 @@ typedef struct {
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ParametersTestPosition parametersTestPosition[] = {
-    //                                                                 | <---------X---------> | <---------Y---------> |
-    {.positionTested = {.X = 0, .Y = 0 },           .expectedResult = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }},
-    {.positionTested = {.X = -1, .Y = 1 },          .expectedResult = { 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x01 }},
-    {.positionTested = {.X = 1, .Y = -1 },          .expectedResult = { 0x00, 0x00, 0x00, 0x01, 0xFF, 0xFF, 0xFF, 0xFF }},
-    {.positionTested = {.X = -12345, .Y = 12345 },  .expectedResult = { 0xFF, 0xFF, 0xCF, 0xC7, 0x00, 0x00, 0x30, 0x39 }},
-    {.positionTested = {.X = 12345, .Y = -12345 },  .expectedResult = { 0x00, 0x00, 0x30, 0x39, 0xFF, 0xFF, 0xCF, 0xC7 }},
+ParametersTestBeacon parametersTestBeacon[] = {
+    // Beacon A
+    {
+        .beaconTested = {
+            .ID = "Aa",
+            .position = {.X = 12345, .Y = -12345 },
+            .power = 12345,
+            .attenuationCoefficient = 1.05,
+            .nbCoefficientAttenuations = 3,                     // Fixed in the prod code
+            .attenuationCoefficients = { -1.05, 1, -1 }
+        },
+        .expectedResult = {
+            0x41, 0x61, 0x00,                                   // ID
+            0x00, 0x00, 0x30, 0x39,                             // position X
+            0xFF, 0xFF, 0xCF, 0xC7,                             // position Y
+            0x00, 0x00, 0x30, 0x39,                             // power
+            0x3F, 0x86, 0x66, 0x66,                             // attenuationCoefficient
+            0x03,                                               // NB attenuationCoefficients calculated
+            0xBF, 0x86, 0x66, 0x66,                             // attenuationCoefficients calculated 1
+            0x3F, 0x80, 0x00, 0x00,                             // attenuationCoefficients calculated 2
+            0xBF, 0x80, 0x00, 0x00                              // attenuationCoefficients calculated 3
+        }
+    },
+
+    // Beacon B
+    {
+        .beaconTested = {
+            .ID = "#}",
+            .position = {.X = -12345, .Y = 12345 },
+            .power = -12345,
+            .attenuationCoefficient = 1,
+            .nbCoefficientAttenuations = 3,                     // Fixed in the prod code
+            .attenuationCoefficients = { 666, -666, 0 }
+        },
+        .expectedResult = {
+            0x23, 0x7D, 0x00,                                   // ID
+            0xFF, 0xFF, 0xCF, 0xC7,                             // position X
+            0x00, 0x00, 0x30, 0x39,                             // position Y
+            0xFF, 0xFF, 0xCF, 0xC7,                             // power
+            0x3F, 0x80, 0x00, 0x00,                             // attenuationCoefficient
+            0x03,                                               // NB attenuationCoefficients calculated
+            0x44, 0x26, 0x80, 0x00,                             // attenuationCoefficients calculated 1
+            0xC4, 0x26, 0x80, 0x00,                             // attenuationCoefficients calculated 2
+            0x00, 0x00, 0x00, 0x00                              // attenuationCoefficients calculated 3
+        }
+    },
+
+    // Beacon C
+    {
+        .beaconTested = {
+            .ID = {127, 10, 0},
+            .position = {.X = -0.05, .Y = 0.05 },
+            .power = 0.05,
+            .attenuationCoefficient = -1.05,
+            .nbCoefficientAttenuations = 3,                     // Fixed in the prod code
+            .attenuationCoefficients = { -1, 0, 1 }
+        },
+        .expectedResult = {
+            0x7F, 0x0A, 0x00,                                   // ID
+            0x00, 0x00, 0x00, 0x00,                             // position X
+            0x00, 0x00, 0x00, 0x00,                             // position Y
+            0x00, 0x00, 0x00, 0x00,                             // power
+            0xBF, 0x86, 0x66, 0x66,                             // attenuationCoefficient
+            0x03,                                               // NB attenuationCoefficients calculated
+            0xBF, 0x80, 0x00, 0x00,                             // attenuationCoefficients calculated 1
+            0x00, 0x00, 0x00, 0x00,                             // attenuationCoefficients calculated 2
+            0x3F, 0x80, 0x00, 0x00                              // attenuationCoefficients calculated 3
+        }
+    },
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -69,6 +132,9 @@ static const struct CMUnitTest tests[] =
     cmocka_unit_test_prestate(test_TRANSLATOR_convertPosition, &(parametersTestPosition[3])),
     cmocka_unit_test_prestate(test_TRANSLATOR_convertPosition, &(parametersTestPosition[4])),
 
+    cmocka_unit_test_prestate(test_TRANSLATOR_convertBeaconData, &(parametersTestBeacon[0])),
+    cmocka_unit_test_prestate(test_TRANSLATOR_convertBeaconData, &(parametersTestBeacon[1])),
+    cmocka_unit_test_prestate(test_TRANSLATOR_convertBeaconData, &(parametersTestBeacon[2]))
 };
 
 int translator_run_tests() {
@@ -97,16 +163,11 @@ static void test_TRANSLATOR_convertPosition(void** state) {
     assert_memory_equal(&result, expectedResult, 8);
 }
 
+static void test_TRANSLATOR_convertBeaconData(void** state) {
+    ParametersTestBeacon* param = (ParametersTestBeacon*) *state;
 
-    /* Test de F */
-    unsigned char result_F[8];
-    TRANSLATOR_convertPosition(&positionTest_F, result_F);
+    unsigned char result[32];
+    TRANSLATOR_convertBeaconData(&(param->beaconTested), result);
 
-    assert_memory_equal(result_F, expectedResult_F, 8);
-
-    /* Test de G */
-    unsigned char result_G[8];
-    TRANSLATOR_convertPosition(&positionTest_G, result_G);
-
-    assert_memory_equal(result_G, expectedResult_G, 8);
+    assert_memory_equal(result, param->expectedResult, 32);
 }
