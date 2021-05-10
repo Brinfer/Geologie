119b869fd1da3eea7d729099115af456d52f216f
gabriel <gabriel.lecenne@reseau.eseo.fr>
2021-05-10 16:34:42 +0200

    #6268 CREAT CODE_I1 receivers.c tests pour déterminer le RSSI

--- production/Geologie/src/Receivers/receivers.c
+++ production/Geologie/src/Receivers/receivers.c
@@ -28,6 +28,56 @@ int rs;
 //hciStatus_t rs2;
 int debug = 0;
 
+int btrssi(char *dest) {
+    int dev_id = -1;
+    struct hci_conn_info_req *cr;
+    bdaddr_t bdaddr;
+    int8_t rssi;
+    int dd;
+
+    str2ba(dest, &bdaddr);
+
+    if (dev_id < 0) {
+        dev_id = hci_for_each_dev(HCI_UP, (void*) btscan, (long) &bdaddr);
+        if (dev_id < 0) {
+             fprintf(stderr, "Not connected.\n");
+             exit(1);
+        }
+    }
+
+        dd = hci_open_dev(dev_id);
+        if (dd < 0) {
+                perror("HCI device open failed");
+                exit(1);
+        }
+
+        cr = malloc(sizeof(*cr) + sizeof(struct hci_conn_info));
+        if (!cr) {
+                perror("Can't allocate memory");
+                exit(1);
+        }
+
+        bacpy(&cr->bdaddr, &bdaddr);
+        cr->type = ACL_LINK;
+        if (ioctl(dd, HCIGETCONNINFO, (unsigned long) cr) < 0) {
+                perror("Get connection info failed");
+                exit(1);
+        }
+
+        if (hci_read_rssi(dd, htobs(cr->conn_info->handle), &rssi, 1000) < 0) {
+                perror("Read RSSI failed");
+                exit(1);
+        }
+
+        printf("RSSI return value: %d\n", rssi);
+
+        free(cr);
+
+        hci_close_dev(dd);
+
+    return 0;
+}
+
 int btconnect(char *dest)
 {
     int rfsock = -1;
@@ -36,15 +86,18 @@ int btconnect(char *dest)
 
     // allocate a socket
     rfsock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    printf("socket %d\n", rfsock);
 
     // set the connection parameters (who to connect to)
     addr.rc_family = AF_BLUETOOTH;
     addr.rc_channel = (uint8_t) 1;
     str2ba( dest, &addr.rc_bdaddr );
+    printf("adresse %s\n", dest);
 
     // connect to server
     if( debug ) printf("Connecting to %s ...\n", dest);
     status = connect(rfsock, (struct sockaddr *)&addr, sizeof(addr));
+    printf("connexion %d\n", status);
 
 
     if( status < 0 ) {
@@ -84,8 +137,9 @@ void btscan(){
         //rs = hci_read_rssi(sock, handle, &rssi, 1000);
         //rs2 = HCI_ReadRssiCmd(handle);
         //printf("rssi : %d\n", rs);
-        btconnect(addresse);
         printf("%s  %s %d\n", addresse, name, rssi);
+        btconnect(addresse);
+        btrssi(addresse);
     }
 
     free( ii );
