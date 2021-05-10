1652e8ff4e2d01de98710bdd76d00738a81c683f
Brinfer <gautierpierrelouis6@gmail.com>
2021-05-10 11:11:19 +0200

    #6208 REVISION GeologieTest, passage des test unitaire en test unitaire parametrer

--- production/Geologie/test/ReceptionistLOG/translator_test.c
+++ production/Geologie/test/ReceptionistLOG/translator_test.c
@@ -33,11 +33,24 @@
 static int set_up(void** state);
 static int tear_down(void** state);
 
+typedef struct {
+    Position positionTested;
+    char expectedResult[8];
+} ParametersTestPosition;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //                                              Variable et structure extern
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ParametersTestPosition parametersTestPosition[] = {
+    //                                                                 | <---------X---------> | <---------Y---------> |
+    {.positionTested = {.X = 0, .Y = 0 },           .expectedResult = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }},
+    {.positionTested = {.X = -1, .Y = 1 },          .expectedResult = { 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x01 }},
+    {.positionTested = {.X = 1, .Y = -1 },          .expectedResult = { 0x00, 0x00, 0x00, 0x01, 0xFF, 0xFF, 0xFF, 0xFF }},
+    {.positionTested = {.X = -12345, .Y = 12345 },  .expectedResult = { 0xFF, 0xFF, 0xCF, 0xC7, 0x00, 0x00, 0x30, 0x39 }},
+    {.positionTested = {.X = 12345, .Y = -12345 },  .expectedResult = { 0x00, 0x00, 0x30, 0x39, 0xFF, 0xFF, 0xCF, 0xC7 }},
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -50,7 +63,12 @@ static void test_TRANSLATOR_convertPosition(void** state);
 
 static const struct CMUnitTest tests[] =
 {
-    cmocka_unit_test(test_TRANSLATOR_convertPosition)
+    cmocka_unit_test_prestate(test_TRANSLATOR_convertPosition, &(parametersTestPosition[0])),
+    cmocka_unit_test_prestate(test_TRANSLATOR_convertPosition, &(parametersTestPosition[1])),
+    cmocka_unit_test_prestate(test_TRANSLATOR_convertPosition, &(parametersTestPosition[2])),
+    cmocka_unit_test_prestate(test_TRANSLATOR_convertPosition, &(parametersTestPosition[3])),
+    cmocka_unit_test_prestate(test_TRANSLATOR_convertPosition, &(parametersTestPosition[4])),
+
 };
 
 int translator_run_tests() {
@@ -68,48 +86,15 @@ static int tear_down(void** state) {
 }
 
 static void test_TRANSLATOR_convertPosition(void** state) {
-    const Position positionTest_A = { .X = 0, .Y = 0 };
-    const Position positionTest_B = { .X = -1, .Y = 1 };
-    const Position positionTest_C = { .X = 1, .Y = -1 };
-    const Position positionTest_D = { .X = -12345, .Y = 12345 };
-    const Position positionTest_E = { .X = 12345, .Y = -12345};
+    ParametersTestPosition* param = (ParametersTestPosition*) *state;
 
-    //                                        | <---------X---------> | <---------Y---------> |
-    const unsigned char expectedResult_A[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-    const unsigned char expectedResult_B[8] = { 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x01 };
-    const unsigned char expectedResult_C[8] = { 0x00, 0x00, 0x00, 0x01, 0xFF, 0xFF, 0xFF, 0xFF };
-    const unsigned char expectedResult_D[8] = { 0xFF, 0xFF, 0xCF, 0xC7, 0x00, 0x00, 0x30, 0x39 };
-    const unsigned char expectedResult_E[8] = { 0x00, 0x00, 0x30, 0x39, 0xFF, 0xFF, 0xCF, 0xC7 };
+    const Position* positionTest = &(param->positionTested);
+    const char* expectedResult = &(param->expectedResult);
 
-    /* Test de A */
-    unsigned char result_A[8];
-    TRANSLATOR_convertPosition(&positionTest_A, result_A);
+    char result[8];
+    TRANSLATOR_convertPosition(positionTest, &result);
 
-    assert_memory_equal(result_A, expectedResult_A, 8);
-
-    /* Test de B */
-    unsigned char result_B[8];
-    TRANSLATOR_convertPosition(&positionTest_B, result_B);
-
-    assert_memory_equal(result_B, expectedResult_B, 8);
-
-    /* Test de C */
-    unsigned char result_C[8];
-    TRANSLATOR_convertPosition(&positionTest_C, result_C);
-
-    assert_memory_equal(result_C, expectedResult_C, 8);
-
-    /* Test de D */
-    unsigned char result_D[8];
-    TRANSLATOR_convertPosition(&positionTest_D, result_D);
-
-    assert_memory_equal(result_D, expectedResult_D, 8);
-
-    /* Test de E */
-    unsigned char result_E[8];
-    TRANSLATOR_convertPosition(&positionTest_E, result_E);
-
-    assert_memory_equal(result_E, expectedResult_E, 8);
+    assert_memory_equal(&result, expectedResult, 8);
 }
 
 
