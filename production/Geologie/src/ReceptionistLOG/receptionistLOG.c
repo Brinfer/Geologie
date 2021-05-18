de60e17e74535144141ab24a461960e531a9819a
Pierre-Louis GAUTIER <pierrelouis.gautier@reseau.eseo.fr>
2021-05-18 17:02:57 +0200

    #6776 Correction erreur merge

--- production/Geologie/src/ReceptionistLOG/receptionistLOG.c
+++ production/Geologie/src/ReceptionistLOG/receptionistLOG.c
@@ -10,11 +10,11 @@
  *
  */
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 //
 //                                              Include
 //
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,12 +31,12 @@
 #include "tools.h"
 #include "receptionistLOG.h"
 #include <string.h>
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include "config.h"
+/////////////////////////////////////////////////////////////////////////////////////////
 //
 //                                              Define
 //
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
 #define IP_SERVER "localhost" //Adresse du pc (pour tester)
 //"192.168.7.1" correspond a l'adresse en usb0 de la carte
@@ -47,33 +47,32 @@
 #define DATA_LENGTH 16
 
 
-typedef char Data[DATA_LENGTH];                         ///TODO changer nom structure
 
-static int keepGoing = 1; /// TODO protect with mutex
+static int keepGoing = 1; // TODO protect with mutex
 static int socketListen;
 static struct sockaddr_in serverAddress;
-static int socketClient;
-// static pthread_t spamThread;
-static pthread_t socketThread;
+static int socketClient = NO_CLIENT_SOCKET_VALUE;
+static pthread_t socketThreadWrite;
+static pthread_t socketThreadListen;
 static pthread_mutex_t mutexSocket = PTHREAD_MUTEX_INITIALIZER;
 
 static void intHandler(int _);
 static int createSocketLOG(void);
 static void configureServerAdressLOG(void);
 static int startServerLOG(void);
-static void* runLOG(void* _);
+static void* receiveFromClient(void* _);
 static int connectToClient(void);
 static int readMsg(Data* data);
 static int readHeaderMsg(Data* data);
 static int disconnectToClient();
-static void spamClientSocket(void);
-static int sendMsg(Data dataToSend);
+static void* sendToClientSocket(void* _);
+static int sendMsg(const Data* dataToSend, const int taille);
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 //
 //                                              Fonctions static
 //
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
 /**
@@ -83,7 +82,7 @@ static int sendMsg(Data dataToSend);
  *
  * @param
  */
-static void intHandler(int _) { /// _ pour dire que ca sera ignore
+static void intHandler(int _) { // _ pour dire que ca sera ignore
     keepGoing = 0;
 }
 
@@ -96,9 +95,9 @@ static int createSocketLOG(void) {
     socketListen = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     int returnValue = EXIT_FAILURE;
 
-    if (socketListen < 0)     {
+    if (socketListen < 0) {
         PRINT("%sError socket%s\n", "\033[41m", "\033[0m");
-    }     else     {
+    } else {
         returnValue = EXIT_SUCCESS;
     }
 
@@ -111,9 +110,9 @@ static int createSocketLOG(void) {
  *
  */
 static void configureServerAdressLOG(void) {
-    serverAddress.sin_family = AF_INET;                               /// Type d'adresse = IP
-    serverAddress.sin_port = htons(PORT_SERVER);                      /// Port TCP ou le service est accessible
-    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);                /// On s'attache a toutes les interfaces
+    serverAddress.sin_family = AF_INET;                               // Type d'adresse = IP
+    serverAddress.sin_port = htons(PORT_SERVER);                      // Port TCP ou le service est accessible
+    serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);                // On s'attache a toutes les interfaces
 }
 
 
@@ -124,31 +123,34 @@ static void configureServerAdressLOG(void) {
  */
 
 static int startServerLOG(void) {
+    PRINT("%s\n", "dans startServerLOG");
     int returnValue = EXIT_FAILURE;
 
     PRINT("%sThe server is serving on port %d at the address %s%s\n",
         "\033[32m", PORT_SERVER, IP_SERVER, "\033[0m");
     PRINT("%sTo shutdown the server press enter%s\n\n", "\033[36m", "\033[0m");
 
-    bind(socketListen, (struct sockaddr*) &serverAddress, sizeof(serverAddress));   /// On attache le socket a l'adresse indiquee
+    bind(socketListen, (struct sockaddr*) &serverAddress, sizeof(serverAddress));   // On attache le socket a l'adresse indiquee
 
-    if (listen(socketListen,1) < 0)     {                                    /// on met le socket en ecoute et on accepte que MAX_PENDING connexions
+    if (listen(socketListen, 1) < 0) {                                    // on met le socket en ecoute et on accepte que 1 connexion
         PRINT("%sError while listenning the port%s\n", "\033[41m", "\033[0m");
-    }     else     {
+    } else {
         returnValue = EXIT_SUCCESS;
     }
 
     return returnValue;
 }
 /**
- * @fn static void* runLOG(void* _) {
- * @brief mthode qui va tourner en boucle pour recevoir lire le socket
+ * @fn static void* receiveFromClient(void* _) {
+ * @brief methode routine qui va tourner en boucle pour recevoir lire le socket
  *
  * @param argc premier argument correspondant au nombre d'arguments
  * @param argv arguments
  */
 
-static void* runLOG(void* _) {
+static void* receiveFromClient(void* _) {
+    PRINT("%s\n", "dans receiveFromClient");
+
     int returnValue = EXIT_FAILURE;
     fd_set env;
 
@@ -157,33 +159,34 @@ static void* runLOG(void* _) {
     while (keepGoing) {
 
         FD_ZERO(&env);
-        FD_SET(STDIN_FILENO, &env);
-        FD_SET(socketListen, &env);
+        FD_SET(STDIN_FILENO, &env);             //on demande a surveiller STDIN_FILENO, entrée standard pour le terminal
+        FD_SET(socketListen, &env);             //on demande a surveiller socket, si qqn touche au socket
 
         pthread_mutex_lock(&mutexSocket);
         int socketClientValue = socketClient;
         pthread_mutex_unlock(&mutexSocket);
-        if (socketClientValue != NO_CLIENT_SOCKET_VALUE){
-            FD_SET(socketClientValue, &env);
+        if (socketClientValue != NO_CLIENT_SOCKET_VALUE) {
+            FD_SET(socketClientValue, &env);        //on surveille que le client ecrit
         }
 
 
-        if (select(FD_SETSIZE, &env, NULL, NULL, NULL) == -1)         {
+        if (select(FD_SETSIZE, &env, NULL, NULL, NULL) == -1) {
             PRINT("%sError with %sselect()%s\n", "\033[41m", "\033[21m", "\033[0m");
-            break; ///keepGoing
+            break; //keepGoing
         }
-
-        if (FD_ISSET(STDIN_FILENO, &env))         {
+        if (FD_ISSET(socketListen, &env)) {                     //si on detecte une connexion 
+            PRINT("%s\n", "Le socket d'ecoute a été réveillé");
+            returnValue = connectToClient();
+            if (returnValue != EXIT_SUCCESS) {
+                break; //keepGoing
+            }
+        } else if (FD_ISSET(STDIN_FILENO, &env)) {
             PRINT("\n%sAsk for exit%s\n", "\033[41m", "\033[0m");
             returnValue = EXIT_SUCCESS;
-            break; ///keepGoing
-        }
-        else if (FD_ISSET(socketListen, &env))         {
-            returnValue = connectToClient();
-            if (returnValue != EXIT_SUCCESS)             {
-                break; ///keepGoing
-            }
-        } else         {
+            break; //keepGoing
+        } else if (FD_ISSET(socketClientValue, &env)) {    //si qqchose ecrit dessus
+            PRINT("%s\n", "Ecriture detectee");
+
             pthread_mutex_lock(&mutexSocket);
             int socketClientValue = socketClient;
             pthread_mutex_unlock(&mutexSocket);
@@ -192,7 +195,7 @@ static void* runLOG(void* _) {
                     static Data previousData[] = { 0 };
                     Data noData[60] = { 0 };
                     //Data headerData[6]={0}; //en cours d'ecriture, pour lire une partie de la trame et ainsi identifier sa nature, taille ...
-                    //returnValue = readHeaderMsg(headerData);
+                    //returnValue = readHeaderMsg(headerData);                    
                     Data data[60] = { 0 };             //remise a zero de data
                     returnValue = readMsg(data);
                     sendMsg(data, sizeof(data));
@@ -209,18 +212,27 @@ static void* runLOG(void* _) {
                     }
                     strcpy(previousData, data);
                 }
+            } else {
+                break;
             }
 
         }
+
     }
+
 
     pthread_mutex_lock(&mutexSocket);
     int socketClientValue = socketClient;
     pthread_mutex_unlock(&mutexSocket);
-    if (socketClientValue != NO_CLIENT_SOCKET_VALUE)         {
+
+
+
+    //int socketClientValue = socketClient;
+
+    if (socketClientValue != NO_CLIENT_SOCKET_VALUE) {
         returnValue += disconnectToClient();
     }
-
+    PRINT("%s \n", " on exit le thread d'ecoute");
     pthread_exit(&returnValue);
 }
 
@@ -231,6 +243,9 @@ static void* runLOG(void* _) {
  */
 
 static int connectToClient(void) {
+    signal(SIGINT, intHandler);
+    PRINT("%s\n", "dans connectToClient");
+
     int returnValue = EXIT_FAILURE;
 
     pthread_mutex_lock(&mutexSocket);
@@ -260,15 +275,15 @@ static int connectToClient(void) {
 }
 
 /**
- * @fn static int readMsg()
- * @brief mthode pour lire contenu socket
+ * @fn static int readMsg(Data* data)
+ * @brief methode pour lire contenu du socket
  *
  * @param data chaine qui prendra les donnees reçues
  */
-static int readMsg() {
-    int returnValue = EXIT_FAILURE;
+static int readMsg(Data* data) {
+    PRINT("%s\n", "dans readMsg");
 
-    Data data;
+    int returnValue = EXIT_SUCCESS;
 
     int quantityToRead = 60;
 
@@ -306,9 +321,8 @@ static int readHeaderMsg(Data* headerData) {
 
 /**
  * @fn static int disconnectToClient()
- * @brief mthode pour se dconnecter d'un client
+ * @brief methode pour se deconnecter d'un client
  *
- * @param clientIndex correspond à l'index du client (2 clients max)
  */
 static int disconnectToClient() {
     PRINT("%s\n", "dans disconnectClient");
@@ -319,10 +333,10 @@ static int disconnectToClient() {
     int returnValue = close(socketClient);
     pthread_mutex_unlock(&mutexSocket);
 
-    if (returnValue < 0)     {
+    if (returnValue < 0) {
         returnValue = EXIT_FAILURE;
-    }     else     {
-        PRINT("%s%sClient is disconnect%s\n\n", "\033[43m", "\033[30m","\033[0m");
+    } else {
+        PRINT("%s%sClient is disconnect%s\n\n", "\033[43m", "\033[30m", "\033[0m");
         pthread_mutex_lock(&mutexSocket);
         socketClient = NO_CLIENT_SOCKET_VALUE;
         pthread_mutex_unlock(&mutexSocket);
@@ -334,13 +348,14 @@ static int disconnectToClient() {
 }
 
 
-/// s'attend à recevoir un pointeur
-/// static void* spamClientSocket(void* _)
+// s'attend à recevoir un pointeur
+// static void* sendToClientSocket(void* _)
 
 /**
- * @fn static void spamClientSocket(void)
+ * @fn static void sendToClientSocket(void)
  * @brief mthode pour envoyer des messages au client de facon repetitive
  *
+ * @param _
  */
 
 static void* sendToClientSocket(void* data) {            //tant que le prog fonctionne on continue
@@ -356,17 +371,14 @@ static void* sendToClientSocket(void* data) {            //tant que le prog fonc
         pthread_mutex_unlock(&mutexSocket);
 
         if (socketClientValue != NO_CLIENT_SOCKET_VALUE) {  // si pas client on envoie pas message
-            returnValue = sendMsg(dataToSend, sizeof(dataToSend));                       // on envoie un message au client
+            returnValue = sendMsg(dataToSend, sizeof(dataToSend));                       // on envoie un message au client 
             if (returnValue != EXIT_SUCCESS) {
                 break; //keepGoing
             }
         }
 
-        if (returnValue != EXIT_SUCCESS)             {
-            break; ///keepGoing
-        }
+        sleep(3);
 
-        sleep(1);
     }
 
     pthread_exit(&returnValue);
@@ -376,7 +388,8 @@ static void* sendToClientSocket(void* data) {            //tant que le prog fonc
  * @fn static int sendMsg(const Data* dataToSend, const int taille)
  * @brief methode pour envoyer des messages au client
  *
- * @param clientIndex correspond à l'index du client auquel on va envoyer (2 clients max)
+ * @param dataToSend information a envoyer
+ * @param taille taille de l'information a envoyer
  */
 
 static int sendMsg(const Data* dataToSend, const int taille) { //dataToSend etant adresse de notre tableau
@@ -385,48 +398,36 @@ static int sendMsg(const Data* dataToSend, const int taille) { //dataToSend etan
 
     int quantityToWrite = taille;
 
-    while (quantityToWrite > 0)     {   ///pas garantie qu'on crit tous les octets
-        pthread_mutex_lock(&mutexSocket);
-        quantityWritten = write(socketClient, &data + quantityWritten, quantityToWrite);
-        pthread_mutex_unlock(&mutexSocket);
 
     pthread_mutex_lock(&mutexSocket);
     write(socketClient, dataToSend, quantityToWrite);
     pthread_mutex_unlock(&mutexSocket);
 
-    if (quantityToWrite == 0)     {
-        returnValue = EXIT_SUCCESS;
-    }
     return returnValue;
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 //
 //                                              Fonctions publiques
 //
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
-/**
- * @fn int ReceptionistLOG_new()
- * @brief fonction qui crer le socket
- *
- */
-extern int ReceptionistLOG_new(void) {                                                                /// configuration socket
+
+extern int ReceptionistLOG_new(void) {                                                                // configuration socket
+    PRINT("%s \n", "dans ReceptionistLOG_new");
+
     int returnValue = EXIT_FAILURE;
 
-    returnValue = createSocketLOG();                                                         /// on crer le socket : AF_INET = IP, SOCK_STREAM = TCP
-    if (returnValue == EXIT_SUCCESS)     {
+    returnValue = createSocketLOG();                                                         // on crer le socket : AF_INET = IP, SOCK_STREAM = TCP
+    if (returnValue == EXIT_SUCCESS) {
         configureServerAdressLOG();
     }
     return returnValue;
 }
 
-/**
- * @fn int ReceptionistLOG_start()
- * @brief fonction principale de receptionistLOG.c qui sera appelee de l'exterieur
- *  pour l'instant
- */
+
 extern int ReceptionistLOG_start(void) {
+    PRINT("%s \n", "dans ReceptionistLOG_start");
     int returnValue = EXIT_FAILURE;
     signal(SIGINT, intHandler);                                                             // Si pb avec ctr c
 
@@ -461,7 +462,6 @@ extern int ReceptionistLOG_start(void) {
     */
     disconnectToClient();
     pthread_mutex_destroy(&mutexSocket);
-
     return returnValue;
 }
 
