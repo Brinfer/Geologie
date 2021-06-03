35c50076e088751486bad77fd96b8b91150fa6e8
Pierre-Louis GAUTIER <pierrelouis.gautier@reseau.eseo.fr>
2021-06-03 10:28:05 +0200

    #6720 CREAT com_common.h, definition des types commun a la comm de Geologie

---
+++ production/Geologie/src/CommGeologie/com_common.h
@@ -0,0 +1,40 @@
+/**
+ * @file com_common.h
+ *
+ * @brief Fichier contenant les types en commun dans le module dédier à la communication de Geologie
+ *
+ * @version 2.0
+ * @date 03-06-2021
+ * @author GAUTIER Pierre-Louis
+ * @copyright Geo-Boot
+ * @license BSD 2-clauses
+ */
+
+#ifdef COM_COMMON_
+#define COM_COMMON_
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//                                              Define
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @struct Header
+ * @brief  TODO
+ *
+ */
+typedef struct {
+    /* TODO */
+} Header;
+
+/**
+ * @typedef Commande
+ *
+ * @brief Enumeration contenant l'identifiant des differentes commandes.
+ */
+typedef enum {
+    /* TODO */
+} Commande;
+
+#endif // COM_COMM_
