ed4c2f15f1575a90b59cb7cbf7618dd8f2be5183
Brinfer <gautierpierrelouis6@gmail.com>
2021-06-03 10:28:05 +0200

    #6720 CREAT com_common.h, definition des types commun a la comm de Geologie

--- production/Geologie/src/config.h
+++ production/Geologie/src/config.h
@@ -32,28 +32,6 @@
 #define SIZE_BEACON_ID (3)
 #define NB_CALIBRATION_POSITIONS (10)
 #define ATT_COEFF_1_METER (6)
-/**
- * @typedef Commande
- *
- * @brief Enumeration contenant l'identifiant des differentes commandes.
- */
-typedef enum {
-    ERROR = -1,
-
-    ASK_CONNECTION = 0,
-    ASK_EXPERIMENTAL_POSITION,
-    ASK_ALL_BEACONS_DATA,
-    ASK_CURRENT_POSITION,
-    ASK_CALIBRATION_POSITION,
-    ASK_MEMORY_PROCESSOR_LOAD,
-
-    SIGNAL_CALIBRATION_START,
-    SIGNAL_CALIBRATION_POSITION,
-    SIGNAL_CALIBRATION_END,
-
-    MIN_VALUE = 0,
-    MAX_VALUE = SIGNAL_CALIBRATION_END
-} Commande;
 
 /**
  * @struct type
@@ -80,7 +58,7 @@ typedef float BeaconCoefficients[NB_BEACONS];
 
 /**
  * @struct type
- * @brief cration d'une structure qui va prendre les diffeerentes donnees d'une balise
+ * @brief creation d'une structure qui va prendre les differentes donnees d'une balise
  */
 typedef struct {
     char ID[SIZE_BEACON_ID];
