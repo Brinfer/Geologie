8a97e169f60b952ee44b83d7aed63fef64d0de61
Pierre-Louis GAUTIER <pierrelouis.gautier@reseau.eseo.fr>
2021-06-03 17:40:32 +0200

    #6722 CREAT proxyLoggerMOB, creation du header de postman, correction de makefile

--- production/Geologie/src/CommGeologie/PostmanLOG/postmanLOG.h
+++ production/Geologie/src/CommGeologie/PostmanLOG/postmanLOG.h
@@ -5,9 +5,9 @@
  *
  * Joue le rôle de serveur dans cette communication et ecoute  le socket pour etablir une
  * connexion ou recevoir une communication.
- * #PostmanLOG ne sait pas décrypter les messages qu’il envoie ou qu’il reçoit.
+ * PostmanLOG ne sait pas décrypter les messages qu’il envoie ou qu’il reçoit.
  * Il s’occupe uniquement de donner les messages qu’il reçoit a DispatcherLOG ainsi que
- * d’envoyer les messages fournis par ProxyGUI et ProxyLoggerMOB
+ * d’envoyer les messages fournis par ProxyGUI et ProxyLoggerMOB.
  *
  * @version 2.0
  * @date 03-06-2021
@@ -20,50 +20,50 @@
 #define POSTMAN_LOG_
 
 /**
- * @brief Initialise le sockect
+ * @brief Initialise le sockect.
  *
- * @return int 0 en cas de succes, une autre valeur sinon
+ * @return int 0 en cas de succes, une autre valeur sinon.
  */
 extern int PostmanLOG_new(void);
 
 /**
- * @brief Ouvre le socket
+ * @brief Ouvre le socket.
  *
  * Reste en attente de connexion si aucune n'est etablie, et permet
  * de lire par la suite des messages.
  *
- * @return int 0 en cas de succes, une autre valeur sinon
+ * @return int 0 en cas de succes, une autre valeur sinon.
  */
 extern int PostmanLOG_start(void);
 
 /**
- * @brief Envoie un message
+ * @brief Envoie un message.
  *
- * @param trame La trame a envoyer
- * @param size La taille de la trame a envoyer
- * @return int 0 en cas de succes, une autre valeur sinon
+ * @param trame La trame a envoyer.
+ * @param size La taille de la trame a envoyer.
+ * @return int 0 en cas de succes, une autre valeur sinon.
  */
 extern int PostmanLOG_sendMsg(const unsigned char* trame, short size);
 
 /**
- * @brief Lie les message sur le socket
+ * @brief Lie les message sur le socket.
  *
- * @param destTrame La trame ou ecrire le message lue
- * @return int 0 en cas de succes, une autre valeur sinon
+ * @param destTrame La trame ou ecrire le message lue.
+ * @return int 0 en cas de succes, une autre valeur sinon.
  */
 extern int PostmanLOG_readMsg(unsigned char* destTrame);
 
 /**
- * @brief Fermeture du socket
+ * @brief Fermeture du socket.
  *
- * @return int 0 en cas de succes, une autre valeur sinon
+ * @return int 0 en cas de succes, une autre valeur sinon.
  */
 extern int PostmanLOG_stop(void);
 
 /**
- * @brief Ferme le socket
+ * @brief Ferme le socket.
  *
- * @return int 0 en cas de succes, une autre valeur sinon
+ * @return int 0 en cas de succes, une autre valeur sinon.
  */
 extern int PostmanLOG_free(void);
 
