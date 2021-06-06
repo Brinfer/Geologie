55f346d6688ba926f3ba5ec73c157d4c184fe8aa
gabriel <gabriel.lecenne@reseau.eseo.fr>
2021-06-06 16:37:44 +0200

    #7536 REVISION CODE_I2 implementation du scan

--- production/Geologie/src/Receiver/receiver.c
+++ production/Geologie/src/Receiver/receiver.c
@@ -16,11 +16,18 @@
 
  */
 
-#include "receiver.h"
-
-#include <stdio.h>
-
 #include <stdlib.h>
+#include <errno.h>
+#include <curses.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+#include <inttypes.h>
+
+#include "receiver.h"
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -50,7 +57,6 @@ typedef enum {
     E_STOP = 0,
     E_ASK_BEACONS_SIGNAL,
     E_MAJ_BEACONS_CHANNEL,
-    E_TRANSLATE_BEACONS_CHANNELS,
     NB_EVENT_RECEIVER
 } Event_RECEIVER;
 
@@ -59,7 +65,6 @@ typedef enum {
     A_STOP,
     A_SEND_BEACONS_SIGNAL,
     A_MAJ_BEACONS_CHANNELS,
-    A_TRANSLATE_BEACONS_CHANNELS,
     NB_ACTION_RECEIVER
 } Action_RECEIVER;
 
@@ -73,10 +78,22 @@ static Transition_RECEIVER stateMachine[NB_STATE - 1][NB_EVENT_RECEIVER] =
     [S_SCANNING] [E_STOP] = {S_DEATH, A_STOP},
     [S_SCANNING] [E_ASK_BEACONS_SIGNAL] = {S_SCANNING, A_SEND_BEACONS_SIGNAL},
     [S_SCANNING] [E_MAJ_BEACONS_CHANNEL] = {S_SCANNING, A_MAJ_BEACONS_CHANNELS},
-    [S_SCANNING] [E_TRANSLATE_BEACONS_CHANNELS] = {S_TRANSLATING, A_TRANSLATE_BEACONS_CHANNELS},
     [S_TRANSLATING] [E_STOP] = {S_DEATH, A_STOP},
     [S_TRANSLATING] [E_MAJ_BEACONS_CHANNEL] = {S_SCANNING, A_MAJ_BEACONS_CHANNELS}
 };
+
+struct hci_request ble_hci_request(uint16_t ocf, int clen, void * status, void * cparam)
+{
+	struct hci_request rq;
+	memset(&rq, 0, sizeof(rq));
+	rq.ogf = OGF_LE_CTL;
+	rq.ocf = ocf;
+	rq.cparam = cparam;
+	rq.clen = clen;
+	rq.rparam = status;
+	rq.rlen = 1;
+	return rq;
+}
 
 State_RECEIVER myState;
 
@@ -91,17 +108,7 @@ State_RECEIVER myState;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/**
 
- * @fn static void Receiver_getAllBeaconsChannel()
-
- * @brief methode privee permettant de memoriser dans beaconsChannel l'ensemble des trames des balises
-
- */
-
-static void Receiver_getAllBeaconsChannel(){
-
-}
 
 /**
 
@@ -114,22 +121,130 @@ static void Receiver_getAllBeaconsChannel(){
  */
 
 static void Receiver_translateChannelToBeaconsSignal(BeaconsChannel channel[]){
-    Event_RECEIVER myEvent = E_TRANSLATE_BEACONS_CHANNELS;
-    Receiver_run(myEvent);
+    //for each -> translatorBeacon
 }
 
 /**
 
- * @fn static void Receiver_run() TODO
+ * @fn static void Receiver_getAllBeaconsChannel()
+
+ * @brief methode privee permettant de memoriser dans beaconsChannel l'ensemble des trames des balises
 
  */
 
-static void Receiver_run(Event_RECEIVER event){
+static void Receiver_getAllBeaconsChannel(){
+    int ret, status;
 
-    Action_RECEIVER action = stateMachine[myState][event].action;
-	myState =  stateMachine[myState][event].destinationState;
-    Receiver_performAction(action);
+	// Get HCI device.
 
+	const int device = hci_open_dev(hci_get_route(NULL));
+	if ( device < 0 ) { 
+		perror("Failed to open HCI device.");
+	}
+
+	// Set BLE scan parameters.
+	
+	le_set_scan_parameters_cp scan_params_cp;
+	memset(&scan_params_cp, 0, sizeof(scan_params_cp));
+	scan_params_cp.type 			= 0x00; 
+	scan_params_cp.interval 		= htobs(0x0010);
+	scan_params_cp.window 			= htobs(0x0010);
+	scan_params_cp.own_bdaddr_type 	= 0x00; // Public Device Address (default).
+	scan_params_cp.filter 			= 0x00; // Accept all.
+
+
+	struct hci_request scan_params_rq = ble_hci_request(OCF_LE_SET_SCAN_PARAMETERS, LE_SET_SCAN_PARAMETERS_CP_SIZE, &status, &scan_params_cp);
+	
+	ret = hci_send_req(device, &scan_params_rq, 1000);
+	if ( ret < 0 ) {
+		hci_close_dev(device);
+		perror("Failed to set scan parameters data.");
+	}
+
+	// Set BLE events report mask.
+
+	le_set_event_mask_cp event_mask_cp;
+	memset(&event_mask_cp, 0, sizeof(le_set_event_mask_cp));
+	int i = 0;
+	for ( i = 0 ; i < 8 ; i++ ) event_mask_cp.mask[i] = 0xFF;
+
+	struct hci_request set_mask_rq = ble_hci_request(OCF_LE_SET_EVENT_MASK, LE_SET_EVENT_MASK_CP_SIZE, &status, &event_mask_cp);
+	ret = hci_send_req(device, &set_mask_rq, 1000);
+	if ( ret < 0 ) {
+		hci_close_dev(device);
+		perror("Failed to set event mask.");
+	}
+
+	// Enable scanning.
+
+	le_set_scan_enable_cp scan_cp;
+	memset(&scan_cp, 0, sizeof(scan_cp));
+	scan_cp.enable 		= 0x01;	// Enable flag.
+	scan_cp.filter_dup 	= 0x00; // Filtering disabled.
+
+	struct hci_request enable_adv_rq = ble_hci_request(OCF_LE_SET_SCAN_ENABLE, LE_SET_SCAN_ENABLE_CP_SIZE, &status, &scan_cp);
+	ret = hci_send_req(device, &enable_adv_rq, 1000);
+	if ( ret < 0 ) {
+		hci_close_dev(device);
+		perror("Failed to enable scan.");
+	}
+
+	// Get Results.
+
+	struct hci_filter nf;
+	hci_filter_clear(&nf);
+	hci_filter_set_ptype(HCI_EVENT_PKT, &nf);
+	hci_filter_set_event(EVT_LE_META_EVENT, &nf);
+	if ( setsockopt(device, SOL_HCI, HCI_FILTER, &nf, sizeof(nf)) < 0 ) {
+		hci_close_dev(device);
+		perror("Could not set socket options\n");
+	}
+
+	printf("Scanning....\n");
+
+	uint8_t buf[HCI_MAX_EVENT_SIZE];
+	evt_le_meta_event * meta_event;
+	le_advertising_info * info;
+	int len;
+
+	while(1) {
+		len = read(device, buf, sizeof(buf));
+		if ( len >= HCI_EVENT_HDR_SIZE ) {
+			meta_event = (evt_le_meta_event*)(buf+HCI_EVENT_HDR_SIZE+1);
+			if ( meta_event->subevent == EVT_LE_ADVERTISING_REPORT ) {
+				uint8_t reports_count = meta_event->data[0];
+				void * offset = meta_event->data + 1;
+				while ( reports_count-- ) {
+					info = (le_advertising_info *)offset;
+					char addr[18];
+					if ( ret < 0 ) {
+						hci_close_dev(device);
+						perror("Failed to enable scan.");
+					}
+					ba2str(&(info->bdaddr), addr);
+
+        //checkUUID
+        //if OK -> Translate
+
+					offset = info->data + info->length + 2;
+				}
+			}
+		}
+	}
+
+	// Disable scanning.
+
+	memset(&scan_cp, 0, sizeof(scan_cp));
+	scan_cp.enable = 0x00;	// Disable flag.
+
+	struct hci_request disable_adv_rq = ble_hci_request(OCF_LE_SET_SCAN_ENABLE, LE_SET_SCAN_ENABLE_CP_SIZE, &status, &scan_cp);
+	ret = hci_send_req(device, &disable_adv_rq, 1000);
+	if ( ret < 0 ) {
+		hci_close_dev(device);
+		perror("Failed to disable scan.");
+	}
+
+	hci_close_dev(device);
 }
 
 /**
@@ -150,13 +265,7 @@ static void Receiver_performAction(Action_RECEIVER action){
 
         case A_SEND_BEACONS_SIGNAL:
             //Scanner_setAllBeaconsSignal();
-            //Thread getChannel()
-            
-            break;
-
-        case A_TRANSLATE_BEACONS_CHANNELS:
-            Receiver_translateChannelToBeaconsSignal(beaconsChannel);
-            //Thread getChannel()
+            //Thread getChannel()         
             break;
 
         case A_MAJ_BEACONS_CHANNELS:
@@ -168,6 +277,20 @@ static void Receiver_performAction(Action_RECEIVER action){
         default:
             break;
     }
+
+}
+
+/**
+
+ * @fn static void Receiver_run() TODO
+
+ */
+
+static void Receiver_run(Event_RECEIVER event){
+
+    Action_RECEIVER action = stateMachine[myState][event].action;
+	myState =  stateMachine[myState][event].destinationState;
+    Receiver_performAction(action);
 
 }
 
@@ -183,10 +306,11 @@ static void Receiver_performAction(Action_RECEIVER action){
 
 
 extern void Receiver_new(){
-    myState = S_SCANNING;
+    myState = S_DEATH;
 }
 
 extern void Receiver_ask4StartReceiver(){
+    myState = S_SCANNING;
     Event_RECEIVER myEvent = E_ASK_BEACONS_SIGNAL;
     Receiver_run(myEvent);
 }
@@ -202,5 +326,13 @@ extern void Receiver_ask4BeaconsSignal(){
 }
 
 extern void Receiver_free(){
-
+    myState = S_DEATH;
 }
+
+/*int main(){
+    Receiver_getAllBeaconsChannel();
+    sleep(10);
+    Receiver_translateChannelToBeaconsSignal(beaconsChannel);
+    Receiver_ask4BeaconsSignal();
+    return 0;
+}*/
