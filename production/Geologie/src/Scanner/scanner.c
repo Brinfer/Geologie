791d48b7719c094a5c31f9be3cbca992a0ae696b
Nathan BRIENT <nathan.brient@reseau.eseo.fr>
2021-06-08 14:49:28 +0200

    #6727 REVISION CODE_I2 modification geographer suite a mise en commun des codes

---
+++ production/Geologie/src/Scanner/scanner.c
@@ -0,0 +1,44 @@
+/**
+ * @file scanner.c
+ *
+ * @version 1.0
+ * @date 08/06/21
+ * @author Nathan BRIENT
+ * @copyright BSD 2-clauses
+ *
+ */
+
+#include "scanner.h"
+
+extern int Scanner_new(){
+    return 0;
+}
+
+extern int Scanner_free(){
+    return 0;
+}
+
+extern int Scanner_askStartScanner(){
+    return 0;
+}
+
+extern int Scanner_askStopScanner(){
+    return 0;
+}
+
+extern int Scanner_askUpdateAttenuationCoefficientFromPosition(CalibrationPosition calibrationPosition){
+    return 0;
+}
+
+
+extern int Scanner_askAverageCalcul(){
+    return 0;
+}
+
+extern int Scanner_setAllBeaconsSignal(BeaconSignal beaconsSignal){
+    return 0;
+}
+
+extern int Scanner_setCurrentProcessorAndMemoryLoad(ProcessorAndMemoryLoad currentProcessorAndMemoryLoad){
+    return 0;
+}
