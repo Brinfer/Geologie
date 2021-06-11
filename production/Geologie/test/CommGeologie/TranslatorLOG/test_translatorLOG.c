9ff7a093ff3568954ba3afc05437d6fc3a1c380d
Brinfer <gautierpierrelouis6@gmail.com>
2021-06-11 11:26:04 +0200

    #6725 REVISION CODE_I2, ajout des tests unitaire pour SignalCalibrationPosition

--- production/Geologie/test/CommGeologie/TranslatorLOG/test_translatorLOG.c
+++ production/Geologie/test/CommGeologie/TranslatorLOG/test_translatorLOG.c
@@ -27,7 +27,7 @@
 /**
  * @brief Nombre de suites de tests a excuter.
  */
-#define NB_SUITE_TESTS_TRANSLATOR_LOG (8)
+#define NB_SUITE_TESTS_TRANSLATOR_LOG (9)
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -87,9 +87,16 @@ extern int32_t test_TranslatorLOG_run_translateForSendCalibrationData(void);
 /**
  * @brief Execute les tests de TranslatorLOG_translateForRepCalibrationPosition.
  *
- * @return int 0 en cas de succes, le numero du test qui a echoue sinon.
+ * @return int32_t 0 en cas de succes, le numero du test qui a echoue sinon.
  */
-extern int test_TranslatorLOG_run_translateForRepCalibrationPosition(void);
+extern int32_t test_TranslatorLOG_run_translateForRepCalibrationPosition(void);
+
+/**
+ * @brief Execute les tests de TranslatorLOG_signalCalibrationPosition.
+ *
+ * @return int32_t 0 en cas de succes, le numero du test qui a echoue sinon.
+ */
+extern int32_t test_TranslatorLOG_run_translateSignalCalibrationPosition(void);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -108,7 +115,8 @@ static int32_t (*suite_tests[])(void) = {
     test_TranslatorLOG_run_translateForSendAllBeaconsData,
     test_TranslatorLOG_run_translateForSendExperimentalTrajects,
     test_TranslatorLOG_run_translateForSendCalibrationData,
-    test_TranslatorLOG_run_translateForRepCalibrationPosition
+    test_TranslatorLOG_run_translateForRepCalibrationPosition,
+    test_TranslatorLOG_run_translateSignalCalibrationPosition
 };
 
 /**
