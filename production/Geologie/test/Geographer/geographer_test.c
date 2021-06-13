cc91aad9b0e149c24bfae4a2c4e89ca55f5348f1
gabriel <gabriel.lecenne@reseau.eseo.fr>
2021-06-13 22:56:18 +0200

    #6732 REVISION CODE_I2 test geographer

--- production/Geologie/test/Geographer/geographer_test.c
+++ production/Geologie/test/Geographer/geographer_test.c
@@ -50,11 +50,7 @@ static pthread_barrier_t barrier_scenario;
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/**
- * @brief Lance la suite de test du module geographer.
- *
- * @return int32_t 0 en cas de succes ou le numero du test qui a echoue.
- */
+
 
 static int32_t setUp(void** state);
 
@@ -77,6 +73,12 @@ static int32_t tearDown(void **state) {
 	return 0;
 }
 
+/**
+ * @brief Lance la suite de test du module geographer.
+ *
+ * @return int32_t 0 en cas de succes ou le numero du test qui a echoue.
+ */
+
 extern int32_t geographer_run_tests();
 
 State_GEOGRAPHER expectedFinalState;
@@ -97,6 +99,11 @@ CalibrationPosition expectedCalibrationPosition;
 CalibrationData * expectedCalibrationData;
 uint8_t expectedNbCalibration;
 
+/**
+ * wrap de la fonction __wrap_Geographer_transitionFct(MqMsg msg)
+ */
+
+
 void __wrap_Geographer_transitionFct(MqMsg msg)
 {
     switch (msg.event)
@@ -107,10 +114,8 @@ void __wrap_Geographer_transitionFct(MqMsg msg)
         expect_function_call(__wrap_ProxyLoggerMOB_stop);
 		expect_function_call(__wrap_ProxyGUI_stop);
 
-        //Is running the real function
         __real_Geographer_transitionFct(msg);
 
-        //Test side effect
         assert_int_equal(expectedFinalState, myState);
 		assert_int_equal(expectedCalibrationCounter, calibrationCounter);
 		assert_int_equal(expectedConnectionState, connectionState);
@@ -123,7 +128,6 @@ void __wrap_Geographer_transitionFct(MqMsg msg)
         expect_function_call(__wrap_ProxyLoggerMOB_setExperimentalPositions);
         expect_function_call(__wrap_ProxyLoggerMOB_setExperimentalTrajects);
 
-		// Vérification paramètre de l'appel à __wrap_mod1_call.
 		expect_value(__wrap_ProxyLoggerMOB_setExperimentalPositions, experimentalPositions, expectedExperimentalPositions);
 		expect_value(__wrap_ProxyLoggerMOB_setExperimentalPositions, EXP_POSITION_NUMBER, expectedNbExperimentalPositions);
 
@@ -198,8 +202,6 @@ void __wrap_Geographer_transitionFct(MqMsg msg)
 
 		expect_function_call(__wrap_Scanner_ask4UpdateAttenuationCoefficientFromPosition);
 
-		//expect_value(__wrap_Scanner_ask4UpdateAttenuationCoefficientFromPosition, calibrationPositions[calibrationCounter], expectedCalibrationPosition);
-
 		__real_Geographer_transitionFct(msg);
 
         assert_int_equal(expectedFinalState, myState);
@@ -256,42 +258,72 @@ void __wrap_Geographer_transitionFct(MqMsg msg)
 
 static pthread_t thread_scenario;
 
+/**
+ * @brief StartCalibrationStop test : \n
+ * Test du scénario qui consite à demarrer Geographer, effectuer le calibrage pour stopper l'objet
+ * 
+ * Le premier test va verifier le comportement de l'objet lors du signal de la connexion etablie avec l'android\n
+ * -> On demarre a l'etat S_WATING_FOR_CONNECTION \n
+ * -> la fonction Geographer_signalConnectionEstablished() intervient et engendre l'evenement E_CONNECTION_ESTABLISHED \n
+ * -> L'etat final est S_IDLE \n
+ * 
+ *  * Le second test va verifier le comportement de l'objet lors du signal de la demande de calibrage\n
+ * -> On demarre a l'etat S_IDLE \n
+ * -> la fonction Geographer_askCalibrationPositions() intervient et engendre l'evenement E_ASK_CALIBRATION_POSITIONS \n
+ * -> L'etat final est S_WAITING_FOR_BE_PLACED \n
+ * 
+ *  * Le troisieme test va verifier le comportement de l'objet lorsque le robot sera palce sur une position de calibrage \n
+ * -> On demarre a l'etat S_WAITING_FOR_BE_PLACED \n
+ * -> la fonction Geographer_validatePosition(CalibrationPositionId) intervient et engendre l'evenement E_VALIDATE_POSITION \n
+ * -> L'etat final est S_WAITING_FOR_ATTENUATION_COEFFICIENT_FROM_POSITION \n
+ * 
+ *  * Les quatieme et cinquieme tests vont verifier le comportement de l'objet lors des calculs des coefficent d'attenuation par position \n
+ * -> L'un si il reste des positions à tester, l'autre si celles-ci ont déjà toutes été essayées 
+ * -> On demarre a l'etat S_WAITING_FOR_ATTENUATION_COEFFICIENT_FROM_POSITION \n
+ * -> la fonction Geographer_signalEndUpdateAttenuation() intervient et engendre l'evenement E_SIGNAL_END_UPDATE_ATTENUATION_CALIBRATION ou E_SIGNAL_END_UPDATE_ATTENUATION_ELSE \n
+ * -> L'etat final est alors S_WAITING_FOR_ATTENUATION_COEFFICIENT_FROM_POSITION ou S_WAITING_FOR_CALCUL_AVERAGE_COEFFICIENT\n
+ * 
+ * * Le sixieme test verifie le comportement a la fin du processus de calibrage
+ * 
+ * * Le septieme verifie la bonne extinction de l'objet Geographer
+ */
+
 static void *test_geographer_startCalibrationStop_scenario(void *arg)
 {
-    // First test
+    // 1er test
     expectedFinalState = S_IDLE;
     myState = S_WATING_FOR_CONNECTION;
     Geographer_signalConnectionEstablished();
     pthread_barrier_wait(&barrier_scenario);
 
-    //Second test
+    //2nd test
     expectedFinalState = S_WAITING_FOR_BE_PLACED;
     myState = S_IDLE;
     Geographer_askCalibrationPositions();
     pthread_barrier_wait(&barrier_scenario);
 
-    //Third test
+    //3eme test
     expectedFinalState = S_WAITING_FOR_ATTENUATION_COEFFICIENT_FROM_POSITION;
     myState = S_WAITING_FOR_BE_PLACED;
     CalibrationPositionId calibrationPosition = 1;
     Geographer_validatePosition(calibrationPosition);
     pthread_barrier_wait(&barrier_scenario);
 
-    //Fourth test
+    //4eme test
     expectedFinalState = S_WAITING_FOR_ATTENUATION_COEFFICIENT_FROM_POSITION;
     myState = S_WAITING_FOR_ATTENUATION_COEFFICIENT_FROM_POSITION;
     calibrationCounter = CALIBRATION_POSITION_NUMBER - 1;
     Geographer_signalEndUpdateAttenuation();
     pthread_barrier_wait(&barrier_scenario);
 
-    //Fifth test
+    //5eme test
     expectedFinalState = S_WAITING_FOR_CALCUL_AVERAGE_COEFFICIENT;
     myState = S_WAITING_FOR_ATTENUATION_COEFFICIENT_FROM_POSITION;
     calibrationCounter = CALIBRATION_POSITION_NUMBER;
     Geographer_signalEndUpdateAttenuation();
     pthread_barrier_wait(&barrier_scenario);
 
-    //Sixth test
+    //6eme test
     expectedFinalState = S_IDLE;
     myState = S_WAITING_FOR_CALCUL_AVERAGE_COEFFICIENT;
     CalibrationData * data;
@@ -299,7 +331,7 @@ static void *test_geographer_startCalibrationStop_scenario(void *arg)
     Geographer_signalEndAverageCalcul(data, nbCalibration);
     pthread_barrier_wait(&barrier_scenario);
 
-    //Seventh test
+    //7eme test
     expectedFinalState = S_DEATH;
     myState = S_IDLE;
     Geographer_askSignalStopGeographer();
@@ -320,7 +352,12 @@ static void test_geographer_startCalibrationStop(void **state)
     expect_function_call(__wrap_ProxyLoggerMOB_start);
     expect_function_call(__wrap_Scanner_ask4StartScanner);
 
-    // Creation & execution of the thread scenario
+    Geographer_askCalibrationPositions();
+
+    Geographer_askSignalStopGeographer();
+    expect_function_call(__wrap_ProxyGUI_stop);
+    expect_function_call(__wrap_ProxyLoggerMOB_stop);
+    expect_function_call(__wrap_Scanner_ask4StopScanner);
 
     if (pthread_create(&thread_scenario, NULL, test_geographer_startCalibrationStop_scenario, NULL) != 0)
     {
@@ -329,13 +366,17 @@ static void test_geographer_startCalibrationStop(void **state)
 
     pthread_detach(thread_scenario);
 
-    // Test thread run the active object
     run(NULL);
 }
 
+/**
+ * @brief connectionDown test : \n
+ * Test du scénario de la perte de connexion
+ */
+
 static void *test_geographer_connectionDown_scenario(void *arg)
 {
-    // First test
+    // Premier test
     expectedFinalState = S_IDLE;
     myState = S_WATING_FOR_CONNECTION;
     Geographer_signalConnectionEstablished();
@@ -362,8 +403,6 @@ static void test_geographer_connectionDown(void **state)
     expect_function_call(__wrap_ProxyLoggerMOB_start);
     expect_function_call(__wrap_Scanner_ask4StartScanner);
 
-    // Creation & execution of the thread scenario
-
     if (pthread_create(&thread_scenario, NULL, test_geographer_connectionDown_scenario, NULL) != 0)
     {
         TRACE("test scenario error %s\n", "pthread_create()");
@@ -371,55 +410,14 @@ static void test_geographer_connectionDown(void **state)
 
     pthread_detach(thread_scenario);
 
-    // Test thread run the active object
-    run(NULL);
-}
-
-static void *test_geographer_datationAndSendingData_scenario(void *arg)
-{
-    // First test
-    expectedFinalState = S_IDLE;
-    myState = S_WATING_FOR_CONNECTION;
-    Geographer_signalConnectionEstablished();
-    pthread_barrier_wait(&barrier_scenario);
-
-    //Second test
-    expectedFinalState = S_IDLE;
-    myState = S_IDLE;
-    Geographer_dateAndSendData();
-    pthread_barrier_wait(&barrier_scenario);
-
-    return NULL;
-}
-
-static void test_geographer_datationAndSendingData(void **state)
-{
-    Geographer_new();
-    expect_function_call(__wrap_ProxyGUI_new);
-    expect_function_call(__wrap_ProxyLoggerMOB_new);
-    expect_function_call(__wrap_Scanner_new);
-
-    Geographer_askSignalStartGeographer();
-    expect_function_call(__wrap_ProxyGUI_start);
-    expect_function_call(__wrap_ProxyLoggerMOB_start);
-    expect_function_call(__wrap_Scanner_ask4StartScanner);
-
-    // Creation & execution of the thread scenario
-
-    if (pthread_create(&thread_scenario, NULL, test_geographer_datationAndSendingData_scenario, NULL) != 0)
-    {
-        TRACE("test scenario error %s\n", "pthread_create()");
-    }
-
-    pthread_detach(thread_scenario);
-
-    // Test thread run the active object
     run(NULL);
 }
 
 static const struct CMUnitTest tests[] =
 {
-    cmocka_unit_test(test_geographer_startCalibrationStop)
+    cmocka_unit_test(test_geographer_startCalibrationStop),
+    cmocka_unit_test(test_geographer_connectionDown),
+
 };
 
 extern int geographer_run_tests(void) {
